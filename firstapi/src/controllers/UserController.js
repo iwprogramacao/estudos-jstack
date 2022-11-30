eaaa06667e25ca15401d148306af0292e072ba41
Igor Wiepieski <iw.programacao@gmail.com>
2022-11-30 19:31:48 -0300

    8ª aula: criando method POST e DELETE

--- firstapi/src/controllers/UserController.js
+++ firstapi/src/controllers/UserController.js
@@ -1,4 +1,4 @@
-const users = require("../mocks/users");
+let users = require("../mocks/users");
 
 /* Aqui ficarão todos os métodos CRUD */
 
@@ -38,5 +38,39 @@ module.exports = {
     users.push(newUser);
 
     response.send(200, newUser);
+  },
+
+  updateUser(request, response) {
+    let { id } = request.params;
+    id = Number(id);
+    const { name } = request.body;
+
+    const userExists = users.find((user) => user.id === id);
+
+    if (!userExists) {
+      return response.send(400, { error: "User not found" });
+    }
+
+    users = users.map((user) => {
+      if (user.id === id) {
+        return {
+          ...user,
+          name,
+        };
+      }
+
+      return user;
+    });
+
+    response.send(200, { id, name });
+  },
+
+  deleteUser(request, response) {
+    let { id } = request.params;
+    id = Number(id);
+
+    users = users.filter((user) => user.id !== id);
+
+    response.send(200, { deleted: true });
   },
 };
