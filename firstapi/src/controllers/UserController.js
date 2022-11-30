470c9b9e842b222027e94a01e126f4bbe8afc590
Igor Wiepieski <iw.programacao@gmail.com>
2022-11-30 18:17:26 -0300

    4ª aula - tratando query params

--- firstapi/src/controllers/UserController.js
+++ firstapi/src/controllers/UserController.js
@@ -4,7 +4,18 @@ const users = require("../mocks/users");
 
 module.exports = {
   listUsers(request, response) {
+    /* Tratando a query */
+    const { order } = request.query;
+
+    const sortedUsers = users.sort((a, b) => {
+      if (order === "desc") {
+        return a.id < b.id ? 1 : -1;
+      }
+
+      return a.id > b.id ? 1 : -1;
+    });
+
     response.writeHead(200, { "Content-Type": "application/json" });
-    response.end(JSON.stringify(users));
+    response.end(JSON.stringify(sortedUsers));
   },
 };
--- firstapi/src/index.js
+++ firstapi/src/index.js
@@ -1,18 +1,29 @@
 const http = require("http");
+const { URL } = require("url");
 const routes = require("./routes");
 
 const server = http.createServer((request, response) => {
-  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+  /**
+   * Separa a URL dos queryParams
+   * - true -> transforma a query de text para object
+   */
+  const parsedUrl = new URL(`http://localhost:3000${request.url}`);
+
+  console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`);
 
   const route = routes.find(
-    (routeObj) => routeObj.method === request.method && routeObj.endpoint === request.url
+    (routeObj) => routeObj.method === request.method && routeObj.endpoint === parsedUrl.pathname
   );
 
   if (route) {
+    /* Injetando a query na request
+        - Object.fromEntries transforma Iterables em objetos */
+    request.query = Object.fromEntries(parsedUrl.searchParams);
+
     route.handler(request, response);
   } else {
     response.writeHead(404, { "Content-Type": "text/html" });
-    response.end(`Cannot ${request.method} ${request.url}`);
+    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
   }
 });
 
