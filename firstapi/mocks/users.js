3d669c8fd61671db4d5ea2d98b5bcb1018b97f8c
Igor Wiepieski <iw.programacao@gmail.com>
2022-11-30 13:53:51 -0300

    2ª aula - Criando a primeira rota da API

---
+++ firstapi/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Igor",
+  },
+  {
+    id: 2,
+    name: "Rose",
+  },
+  {
+    id: 3,
+    name: "José",
+  },
+];
--- firstapi/src/index.js
+++ firstapi/src/index.js
@@ -1,8 +1,24 @@
 const http = require("http");
+const users = require("../mocks/users");
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "text/html" });
-  response.end("<h1>Hello World</h1>");
+  /* Console log da request que está chegando do servidor */
+  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+  /* Ao acessar localhost:3000, responde o seguinte texto no servidor:
+      
+      Request method: GET | Endpoint: /
+      Request method: GET | Endpoint: /favicon.ico
+    
+    Sabendo desses atributos, podemos indicar o que cada requisição irá receber:
+  */
+
+  if (request.method === "GET" && request.url === "/users") {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify(users));
+  } else {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.end("<h1>Hello World</h1>");
+  }
 });
 
 server.listen(3000, () => console.log("💀 Server started at http://localhost:3000"));
